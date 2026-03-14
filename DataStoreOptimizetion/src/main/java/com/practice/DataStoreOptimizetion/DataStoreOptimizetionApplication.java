6981bba5ffdb0f9d23481bdee20945152bdd2047
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2026-03-14 22:25:20 +0700

    Khởi tạo mã nguồn

---
+++ DataStoreOptimizetion/src/main/java/com/practice/DataStoreOptimizetion/DataStoreOptimizetionApplication.java
@@ -0,0 +1,13 @@
+package com.practice.DataStoreOptimizetion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStoreOptimizetionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStoreOptimizetionApplication.class, args);
+	}
+
+}
---
+++ DataStoreOptimizetion/src/test/java/com/practice/DataStoreOptimizetion/DataStoreOptimizetionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.DataStoreOptimizetion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStoreOptimizetionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
